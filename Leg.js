44f0bb1a37fe5050221668226058da15c1318f83
Your Name <you@example.com>
2025-05-30 09:10:17 +0200

    Dodanie budowania 2 dodatkowych blokow oraz naprawa bledu zwiazanego z obracaniem blokow

---
+++ Leg.js
@@ -0,0 +1,12 @@
+function BuildLeg(){
+    LegX = [];
+    LegY = [];
+    LegX[0] = 0;
+    LegY[0] = 0;
+    LegX[1] = 0;
+    LegY[1] = 1;
+    LegX[2] = 0;
+    LegY[2] = 2;
+    LegX[3]= 1;
+    LegY[3] = 2
+}
---
+++ RLeg.js
@@ -0,0 +1,12 @@
+function BuildRleg(){
+    RLegX = [];
+    RLegY = [];
+    RLegX[0] = 1;
+    RLegY[0] = 0;
+    RLegX[1] = 1;
+    RLegY[1] = 1;
+    RLegX[2] = 1;
+    RLegY[2] = 2;
+    RLegX[3] = 0;
+    RLegY[3] = 2;
+}
